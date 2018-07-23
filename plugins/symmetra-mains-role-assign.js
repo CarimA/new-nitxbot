edd3702842ade65eced8d01af4a9e72442c8efff
Carim A <akeju.carim+heroku@gmail.com>
2018-07-23 12:54:57 +0100

    Someday I will learn to remember how string interpolation works

--- plugins/symmetra-mains-role-assign.js
+++ plugins/symmetra-mains-role-assign.js
@@ -19,7 +19,7 @@ var emoji_kr_flag =             '🇰🇷';
 var emoji_pc =                  '🖥';
 var emoji_ps =                  '🇵';
 var emoji_xb =                  '🇽';
-var intro_message =             `Hello! Welcome to Symmetra Mains. Here, we have a few extra roles used to differentiate roles for Overwatch's server regions and platforms, which may be used to find users for games or matchmaking purposes. In order to assign a role to yourself, you need to react to this message.\r\n\r\nReact with {emoji_eu_flag} to be assigned the Europe region role.\r\nReact with {emoji_us_flag} to be assigned the Americas region role.\r\nReact with {emoji_kr_flag} to be assigned the Asia region role.\r\n\r\nReact with {emoji_pc} to be assigned the PC platform role.\r\nReact with {emoji_ps} to be assigned the PlayStation 4 platform role.\r\nReact with {emoji_xb} to be assigned the Xbox One platform role.\r\n\r\nWhile members can have any of these roles assign, please only assign roles that are actually applicable to you.\r\nIf you want a role unassigned, simply unreact (You may need to react and unreact!).`;
+var intro_message =             `Hello! Welcome to Symmetra Mains. Here, we have a few extra roles used to differentiate roles for Overwatch's server regions and platforms, which may be used to find users for games or matchmaking purposes. In order to assign a role to yourself, you need to react to this message.\r\n\r\nReact with ${emoji_eu_flag} to be assigned the Europe region role.\r\nReact with ${emoji_us_flag} to be assigned the Americas region role.\r\nReact with ${emoji_kr_flag} to be assigned the Asia region role.\r\n\r\nReact with ${emoji_pc} to be assigned the PC platform role.\r\nReact with ${emoji_ps} to be assigned the PlayStation 4 platform role.\r\nReact with ${emoji_xb} to be assigned the Xbox One platform role.\r\n\r\nWhile members can have any of these roles assign, please only assign roles that are actually applicable to you.\r\nIf you want a role unassigned, simply unreact (You may need to react and unreact!).`;
 
 
 module.exports = {
@@ -30,25 +30,24 @@ module.exports = {
             client.channels.get(id_room_role).send(intro_message)
                 .then((msg) => {
                     id_role_assign_message = msg.id;
-                })
-                // react with emoji in order to stop people from locking themselves out
-                .then((msg) => {
-                    msg.react(emoji_eu_flag);
-                })
-                .then((msg) => {
-                    msg.react(emoji_us_flag);
-                })
-                .then((msg) => {
-                    msg.react(emoji_kr_flag);
-                })
-                .then((msg) => {
-                    msg.react(emoji_pc);
-                })
-                .then((msg) => {
-                    msg.react(emoji_ps);
-                })
-                .then((msg) => {
-                    msg.react(emoji_xb);
+                    
+                    // react with emoji in order to stop people from locking themselves out
+                    msg.react(emoji_eu_flag)
+                        .then((msg) => {
+                            msg.react(emoji_us_flag);
+                        })
+                        .then((msg) => {
+                            msg.react(emoji_kr_flag);
+                        })
+                        .then((msg) => {
+                            msg.react(emoji_pc);
+                        })
+                        .then((msg) => {
+                            msg.react(emoji_ps);
+                        })
+                        .then((msg) => {
+                            msg.react(emoji_xb);
+                        });
                 });
         });
 
